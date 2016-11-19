9bf5592e811ff77f173148fa3987f6a93aa1a98a
Tobias Beese <tobias.beese@gmx.de>
2016-11-19 22:46:41 +0100

    Add basic bootstrap grid and angular directives

--- chapter2/js/controller.js
+++ chapter2/js/controller.js
@@ -1,5 +1,18 @@
 angular.module('MyApp.controller', []).controller('AppCtrl', function($scope) {
   $scope.clickHandler = function() {
-    window.alert('Clicked!');
+    $scope.isHidden = !$scope.isHidden;
   };
+
+  $scope.contacts = [
+    {
+      name: 'John Doe',
+      phone: '01234567890',
+      email: 'john.doe@example.com'
+    },
+    {
+      name: 'Karen Brownwich',
+      phone: '09876543210',
+      email: 'karen@example.com'
+    }
+  ];
 });
